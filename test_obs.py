adf97df8ebd2e9b647ec028a7f6be0c140a82580
Quentin Delepine <QDelepine@berkeley.edu>
2020-05-07 16:08:36 -0700

    added test_obs

---
+++ test_obs.py
@@ -0,0 +1,22 @@
+import gym
+import time
+from PIL import Image
+from matplotlib import pyplot as plt
+
+
+env = gym.make("procgen:procgen-fruitbot-v0")
+obs = env.reset()
+plt.ion()
+plt.show()
+while True:
+    obs, reward, done, info = env.step(env.action_space.sample())
+    if done:
+        break
+
+    print(obs.shape)
+    env.render()
+    plt.imshow(obs, interpolation='nearest')
+
+    time.sleep(.1)
+
+env.close()
